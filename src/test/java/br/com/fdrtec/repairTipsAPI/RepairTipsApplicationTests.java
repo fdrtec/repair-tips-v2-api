c3beb8ba35a13b388e26b47c3103254a9e129526
fdrtec <fdrtec@gmail.com>
2022-08-28 22:32:07 -0300

    feature: Recriação do esqueleto inicial

---
+++ src/test/java/br/com/fdrtec/repairTipsAPI/RepairTipsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fdrtec.repairTipsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepairTipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
