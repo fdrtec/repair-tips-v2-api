a85fb8fc07ecb5bfc59ad1151221e1039a217d0b
fdrtec <fdrtec@gmail.com>
2022-08-30 22:33:25 -0300

    feat:[sprint-2.6.11] initial skeleton

--- src/main/java/br/com/fdrtec/repairtipsv2/Repairtipsv2Application.java
+++ src/main/java/br/com/fdrtec/repairtipsv2/Repairtipsv2Application.java
@@ -1,13 +1,13 @@
-package br.com.fdrtec.api.repairtips;
+package br.com.fdrtec.repairtipsv2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RepairTipsApiApplication {
+public class Repairtipsv2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RepairTipsApiApplication.class, args);
+		SpringApplication.run(Repairtipsv2Application.class, args);
 	}
 
 }
--- src/test/java/br/com/fdrtec/repairtipsv2/Repairtipsv2ApplicationTests.java
+++ src/test/java/br/com/fdrtec/repairtipsv2/Repairtipsv2ApplicationTests.java
@@ -1,10 +1,10 @@
-package br.com.fdrtec.api.repairtips;
+package br.com.fdrtec.repairtipsv2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RepairTipsApiApplicationTests {
+class Repairtipsv2ApplicationTests {
 
 	@Test
 	void contextLoads() {
