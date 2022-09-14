7aba0a29afb66bc8826445e2f40cada34a58013c
fdrtec <fdrtec@gmail.com>
2022-09-14 00:16:56 -0300

    Inclusão de DTO e DozerMapper

--- src/main/java/br/com/fdrtec/repairtipsv2/controllers/TypeController.java
+++ src/main/java/br/com/fdrtec/repairtipsv2/controllers/TypeController.java
@@ -3,6 +3,8 @@ package br.com.fdrtec.repairtipsv2.controllers;
 import java.util.List;
 import java.util.UUID;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -11,9 +13,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.fdrtec.repairtipsv2.entities.Type;
+import br.com.fdrtec.repairtipsv2.model.dtos.TypeDTO;
+import br.com.fdrtec.repairtipsv2.model.entities.Type;
 import br.com.fdrtec.repairtipsv2.services.TypeService;
 
 @RestController
@@ -36,7 +40,13 @@ public class TypeController {
     }
 
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Type> save(@RequestBody Type type) {
-        return ResponseEntity.ok().body(typeService.save(type));        
+    public ResponseEntity<Type> save(@RequestBody @Valid TypeDTO typeDTO) {
+        return ResponseEntity.ok().body(typeService.save(typeDTO));        
     }
+
+    // @PostMapping
+    // public @ResponseBody Type newType(@Valid Type type) {
+    //     typeService.save(type);
+    //     return type;
+    // }
 }
